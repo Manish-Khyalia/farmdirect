e69b858be70b535ac28d002e832c5432d77aed24
Manish-Khyaliaclear <manishkhyalia2017@gmail.com>
2026-07-18 23:13:50 +0530

    day5: custom exceptions and global exception handler created

---
+++ backend/src/main/java/com/farmdirect/backend/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.farmdirect.backend.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ backend/src/main/java/com/farmdirect/backend/exception/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package com.farmdirect.backend.exception;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
